4c7aea763ce3bc74b9a15d22706d205b9f1f3a72
shar-c0des <ngomakablesharleen703@gmail.com>
2025-12-30 23:12:31 +0200

    feat (LO1): SQLModel practice

--- learning/Libraries/SQLModel/L01.py
+++ learning/Libraries/SQLModel/L01.py
@@ -1,32 +1,72 @@
 # first map sql tables
-
-
+from typing import Optional, List
 # creating a sqlmodel
-from sqlmodel import Field, Session, SQLModel, create_engine, select  # Session and create_engine needed to write to database
+from sqlmodel import Field, Session, SQLModel, Relationship, create_engine, select  # Session and create_engine
+# needed to write to database
 
 
-class Hero(SQLModel, table=True):  # class hero is a sql model equivalent to a sql table
-    # class attributes are equivalent to each column of the table
-    id: int | None = Field(default=None, primary_key=True)
+# define a  class
+class Team(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True)
+    headquarters: str
+
+    # link to the team attr in the hero class
+    heroes: list['Hero'] = Relationship(back_populates="team")
+
+
+# 1. define hero class
+class Hero(SQLModel, table=True):
+    # class hero is a sql model equivalent to a sql table
+    # class attributes are equivalent to each column of the table
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(unique=True)  # unique constraint which prevents duplication
     secret_name: str
-    age: int | None = None 
+    age: int | None = None
+
+    # connect hero to a team id (foreign key link)
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+
+    # link to the heroes attr in the team class
+    team: Optional['Team'] = Relationship(back_populates="heroes")
 
 
 # creating rows
-hero_1 = Hero(name='<NAME>', secret_name='<Name>', age=18)
-hero_2 = Hero(name='Deadpond', secret_name='James Walrich')
-hero_3 = Hero(name='Fran Pire', secret_name='Francois Andrews', age=70)
-hero_4 = Hero(name='The Knight', secret_name='Willy Macantosh', age=18)
-hero_5 = Hero(name='Kitty', secret_name='Jane Sliven', age=23)
-
+hero_data = [
+Hero(name='<NAME>', secret_name='<Name>', age=18),
+Hero(name='Deadpond', secret_name='James Walrich'),
+Hero(name='Fran Pire', secret_name='Francois Andrews', age=70),
+Hero(name='The Knight', secret_name='Willy Macantosh', age=18),
+Hero(name='Kitty', secret_name='Jane Sliven', age=23)
+]
+# initialize an engine to connect to sqlite database 
 engine = create_engine('sqlite:///hero.db')
 
 SQLModel.metadata.create_all(engine)
 
-heroes_to_add = [hero_5]
+with Session(engine) as session:
+    # 1. create the team object
+    team1 = Team(name='The preventers', headquarters='sharp tower')
+
+    # 2. Check if it exists
+    statement = select(Team).where(Team.name == team1.name)
+    existing_team = session.exec(statement).first()
+
+    if not existing_team:
+        session.add(team1)
+        session.commit()
+        session.refresh(team1)
+
+
+# use session to check for existing entries and add new ones without duplicates when rerunning code
+heroes_to_add = hero_data
 
 with Session(engine) as session:
+
+    team = session.exec(
+        select(Team).where(Team.name == 'The preventers')
+
+    ).first()
     for hero in heroes_to_add:
 
         # 1. Search for a hero with this name
@@ -35,6 +75,7 @@ with Session(engine) as session:
 
         # 2. Decision logic
         if not existing_hero:
+            hero.team = team
             session.add(hero)
             print(f'Adding {hero.name} to the database')
         else:
@@ -47,3 +88,6 @@ with Session(engine) as session:
     results = session.exec(statement)
     for hero in results:
         print(hero)
+
+
+                           
