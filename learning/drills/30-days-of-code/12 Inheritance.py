07483dcb3bca76a3550c5fe4897c5c1d07a55a91
shar-c0des <ngomakablesharleen703@gmail.com>
2026-01-03 21:45:27 +0200

    feat (13): inheritance hackerrank solution

---
+++ learning/drills/30-days-of-code/12 Inheritance.py
@@ -0,0 +1,49 @@
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+
+    def printPerson(self):
+        print(f"Name: {self.lastName}, {self.firstName}\nID: {self.idNumber}")
+
+
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, scores):
+
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    def calculate(self):
+        avg = sum(self.scores) / len(self.scores)
+
+        if 90 <= avg <= 100:
+            return 'O'
+
+        elif 80 <= avg < 90:
+            return 'E'
+
+        elif 70 <= avg < 80:
+            return 'A'
+
+        elif 55 <= avg < 70:
+            return 'P'
+
+        elif 40 <= avg < 55:
+            return 'D'
+
+        else:
+            return 'T'
+
+
+if __name__ == "__main__":
+    line = input().split()
+    firstName = line[0]
+    lastName = line[1]
+    idNum = line[2]
+    numScores = int(input())
+    scores = list(map(int, input().split()))
+    s = Student(firstName, lastName, idNum, scores)
+
+    s.printPerson()
+    print("Grade:", s.calculate())
