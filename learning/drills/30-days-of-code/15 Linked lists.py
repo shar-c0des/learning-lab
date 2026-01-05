59d8d0da53c052b02f1f2f28fec65deb8766cd0f
Sharleen Ngomakapile <ngomakapilesharleen703@gmail.com>
2026-01-05 21:36:50 +0200

    Implement linked list with display and insert methods(base template)
    Added Node class and methods for linked list operations.

--- learning/drills/30-days-of-code/15 Linked lists.py
+++ learning/drills/30-days-of-code/15 Linked lists.py
@@ -1,1 +1,26 @@
+class Node:
+  def __init__(self,data):
+    self.data = data 
+    self.next = None
+
+class Soution:
+  def display(self,head):
+    current = head
+    while current:
+      print(current.data,end=''_
+      current = current.next 
+
+  def insert(self,head,data):
+
+
+
+  mylist = Solution()
+  T = int(input())
+  head = None
+  for i in range(T):
+    data = int(input())
+    head=mylist.insert(head,data)
+  mylist.display(head)
+  
+
 
